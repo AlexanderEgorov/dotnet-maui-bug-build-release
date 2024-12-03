c039d26459d9fa3834f2cf749105ae2795998e76
Alexander Egorov <alexander.egorov@devexpress.com>
2024-12-03 12:34:48 +0700

    Initial commit

---
+++ scr/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace DXMauiApp;
+
+public partial class MainPage : ContentPage {
+    public MainPage() {
+        InitializeComponent();
+    }
+}
+
+public class MyControl : Border {
+    public static readonly BindableProperty NullableGridLengthProperty = 
+        BindableProperty.Create(nameof(NullableGridLength), typeof(GridLength?), typeof(MyControl), null);
+    [TypeConverter(typeof(GridLengthTypeConverter))]
+    public GridLength? NullableGridLength { get => (GridLength?)GetValue(NullableGridLengthProperty); set => SetValue(NullableGridLengthProperty, value); }  
+}
